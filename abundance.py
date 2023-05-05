8a7324c1149d008ca53e72b5c1b00651cbf06dcd
Douglas Freeburn <91606877+DougF2308@users.noreply.github.com>
2023-05-05 14:52:40 +0100

    Update abundance.py

--- abundance.py
+++ abundance.py
@@ -4,47 +4,47 @@ import sys
 import os
 
 def main(output_dir, input_file=None, output_file=None):
-    # Initialize a counter to count the occurrences of each gene
+    # counter to count the occurrences of each gene
     gene_counter = Counter()
 
-    # Get the list of promoter directories to process
+    # if no genes to process given
     if input_file is None:
-        # If no input file is given, process all of the directories
+        # If no input file is given, process all of the genes (all genes from output file)
         promoter_dirs = os.listdir(output_dir)
     else:
-        # If an input file is given, read the names from the file and use them to filter the directories
+        # If an input file is given, read the names and use to filter the output directory
         with open(input_file) as f:
             promoter_dirs = [line.strip() for line in f]
 
-    # Iterate over the promoter directories in the output directory
+    # loop over the output directory
     for promoter_dir in os.listdir(output_dir):
         if promoter_dir in promoter_dirs:
             promoter_path = os.path.join(output_dir, promoter_dir)
-            # Iterate over the TF family directories in the promoter directory
+            # loop over the TF family directories in the gene directory
             for tf_family_dir in os.listdir(promoter_path):
                 tf_family_path = os.path.join(promoter_path, tf_family_dir)
-                # Iterate over the peak count files in the TF family directory
+                # loop over the peak count files in the TF family directory
                 for peak_count_file in os.listdir(tf_family_path):
                     # Read the count from the file and add it to the counter
                     with open(os.path.join(tf_family_path, peak_count_file)) as f:
-                        # Iterate over the lines in the file
+                        # loop over the lines in the file
                         for line in f:
-                            # Split the line on the colon character
+                            # Split the line on the colon character to get number of occurances
                             gene, count = re.split(r':\s*', line, maxsplit=1)
                             gene_counter[gene] += int(count)
 
-    # Open the output file in write mode
+    # write output file
     with open(output_file, 'w') as outfile:
-        # Write the gene names and counts to the file
         for gene, count in gene_counter.most_common():
             outfile.write(f'{gene}: {count}\n')
 
+# main program           
 if __name__ == '__main__':
-    # Get the output directory from the command line argument
+    # output directory from the command line
     output_dir = sys.argv[1]
-    # Get the input file name from the command line argument (if it was given)
+    # Get the input file name from the command line
     input_file = sys.argv[2] if len(sys.argv) > 2 else None
-    # Get the output file name from the command line argument (if it was given)
+    # Get the output file name from the command line
     output_file = sys.argv[3] if len(sys.argv) > 3 else 'TF_counts.txt'
     main(output_dir, input_file, output_file)
 
